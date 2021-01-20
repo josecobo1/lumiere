b60bd0fc7b8637f80b905c0d7f89c72299a889af
Jose Cobo <jose@MacBook-Pro-de-Jose.local>
2021-01-20 13:42:24 +0100

    toast if location or api fails on get upcoming movies page

--- src/app/pages/upcoming/upcoming.page.ts
+++ src/app/pages/upcoming/upcoming.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { UpcomingMovies } from 'src/app/core/model/movies/upcoming-movies';
 import { GeolocationService } from 'src/app/core/services/geolocation.service';
 import { MoviesService } from 'src/app/core/services/movies.service';
@@ -14,14 +14,36 @@ export class UpcomingPage implements OnInit {
 
   constructor(private moviesService: MoviesService, 
               public modalController: ModalController,
-              public geolocationService: GeolocationService) { }
+              public geolocationService: GeolocationService,
+              public toastController: ToastController) { }
 
   upcomingMovies: UpcomingMovies;
   region: any;
 
   async ngOnInit() {
-    this.region = await this.geolocationService.getCurrentLocation();
-    this.upcomingMovies = await this.moviesService.getUpcomingMovies$(this.region).pipe().toPromise();
+
+    try {
+      this.region = await this.geolocationService.getCurrentLocation();
+    } catch (error) {
+      this.presentToast(`Can't get your current location`);
+    }
+
+    try {
+      this.upcomingMovies = await this.moviesService.getUpcomingMovies$(this.region).pipe().toPromise();
+    } catch (error) {
+      this.presentToast(`Can't get the upcoming movie listfor your location, please try again later`);
+    }
+
+  }
+
+  async presentToast(message){
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 4000,
+      translucent: true
+    });
+
+    toast.present();
   }
 
   getDetails(movie){
