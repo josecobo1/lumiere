23ea133785a5b7dbf2d8d2ec8a469ad2c982fbd7
Jose Cobo <jose@MacBook-Pro-de-Jose.local>
2021-01-21 11:58:22 +0100

    confirmation toast added on save as seen and save later succes

--- src/app/shared/components/modal-movie-details/modal-movie-details.component.ts
+++ src/app/shared/components/modal-movie-details/modal-movie-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActionSheetController, ModalController } from '@ionic/angular';
+import { ActionSheetController, ModalController, ToastController } from '@ionic/angular';
 import { Movie } from 'src/app/core/model/movies/movie';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { UserService } from 'src/app/core/services/user.service';
@@ -15,7 +15,8 @@ export class ModalMovieDetailsComponent implements OnInit {
   constructor(public modalController: ModalController, 
               public auth: AuthService, 
               public userService: UserService,
-              public actionSheetController: ActionSheetController) { }
+              public actionSheetController: ActionSheetController,
+              public toast: ToastController) { }
 
   @Input() movie: Movie;
   @Input() images: any;
@@ -46,11 +47,20 @@ export class ModalMovieDetailsComponent implements OnInit {
     // Si el usuario ha inicido sesión guardamos la película como vista, en caso contrario la función isLogged 
     // mostrará el modal de inicio de sesión
     if(await this.isUserLogged()){
-      // Recupero el uid del usuario de la base de datos
-      const userUid = await this.auth.getUserId(); 
 
-      // Uso uid el usuario e id de la película para añadir la pelilcula a vistas
-      await this.userService.addMovieToSeen(userUid, this.movie.id);
+      try {
+        // Recupero el uid del usuario de la base de datos
+        const userUid = await this.auth.getUserId(); 
+
+        // Uso uid el usuario e id de la película para añadir la pelilcula a vistas
+        await this.userService.addMovieToSeen(userUid, this.movie.id);
+
+        // Toast de confirmación
+        this.presentToast('Movie saved as seen');
+      } catch (error) {
+        this.presentToast('Oops something has failed please try again later');
+      }
+      
     } 
      
   }
@@ -58,11 +68,20 @@ export class ModalMovieDetailsComponent implements OnInit {
   async setAsToSee(){
 
     if(await this.isUserLogged()) {
-      // Recupero el uid del usuario de la base de datos
-      const userUid = await this.auth.getUserId(); 
 
-      // Uso uid el usuario e id de la película para añadir la pelilcula a vistas
-      await this.userService.addMovieToSeeLater(userUid, this.movie.id);
+      try {
+        // Recupero el uid del usuario de la base de datos
+        const userUid = await this.auth.getUserId(); 
+
+        // Uso uid el usuario e id de la película para añadir la pelilcula a vistas
+        await this.userService.addMovieToSeeLater(userUid, this.movie.id);
+
+        // Muestro Toast de confirmación
+        this.presentToast('Movie saved to save later');
+      } catch (error) {
+        this.presentToast('Ooops something failed please try again later');
+      }
+      
     }
     
   }
@@ -103,6 +122,14 @@ export class ModalMovieDetailsComponent implements OnInit {
 
   dismissModal() {
     this.modalController.dismiss();
+  }
+
+  async presentToast(message) {
+    const toast = await this.toast.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
